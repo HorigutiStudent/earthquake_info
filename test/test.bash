ca0588df8e1f214078db81104fb6998b9ba617f4
HorigutiStudent <s23c1127lc@s.chibakoudai.jp>
2024-11-28 13:37:37 +0900

    test clear

--- test/test.bash
+++ test/test.bash
@@ -7,9 +7,9 @@ dir="/home/RoboSys"
 cd $dir/ros2_ws
 ./colbuild
 source install/setup.bash
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+timeout 3 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log | 
-	grep 'Listen: 10'
+	grep 'Lis: 0'
 
 
