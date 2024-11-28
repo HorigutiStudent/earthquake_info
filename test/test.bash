db775c0e78a24669938319dd7ace3d0bd5fdb7d8
HorigutiStudent <s23c1127lc@s.chibakoudai.jp>
2024-11-28 13:56:25 +0900

    test

--- test/test.bash
+++ test/test.bash
@@ -6,7 +6,7 @@ dir=~
 dir="/home/RoboSys"
 cd $dir/ros2_ws
 ./colbuild
-source install/setup.bash
+source ~/.bashrc
 timeout 3 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log | 
