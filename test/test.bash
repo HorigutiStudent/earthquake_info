798e6a0f560d4545a9e790774b8dce0ac29c0663
HorigutiStudent <s23c1127lc@s.chibakoudai.jp>
2024-12-05 12:03:45 +0900

    クラスを使って書く

--- test/test.bash
+++ test/test.bash
@@ -7,7 +7,7 @@ dir="/home/RoboSys"
 cd $dir/ros2_ws
 ./colbuild
 source ~/.bashrc
-timeout 3 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+timeout 3 ros2 launch mypkg talk_listen.launch.py > tee - /tmp/mypkg.log
 
 cat /tmp/mypkg.log | 
 	grep 'Lis: 0'
