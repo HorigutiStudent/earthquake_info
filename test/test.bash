862ebc4a499695caa3950f157a500c79885e8861
HorigutiStudent <s23c1127lc@s.chibakoudai.jp>
2025-01-09 17:19:03 +0900

    make test

--- test/test.bash
+++ test/test.bash
@@ -7,8 +7,8 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 10 ros2 launch mypkg talk_listen.launch.py | tee -  /tmp/mypkg.log
+timeout 3 ros2 launch mypkg talk_listen.launch.py | tee -  /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
-grep 'Listen: 10'
+grep 'Lis: 3'
 
