b498129639a694e75252774617774d95ee9d10c0
HorigutiStudent <s23c1127lc@s.chibakoudai.jp>
2025-01-09 17:10:57 +0900

    make test

--- test/test.bash
+++ test/test.bash
@@ -1,15 +1,14 @@
 #!/bin/bash
+#TODO : SPDX License 
 
-dir=~
+dir=/home/akage/practice
 [ "$1" != "" ] && dir="$1"
-
-dir="/home/RoboSys"
 cd $dir/ros2_ws
-./colbuild
-source ~/.bashrc
-timeout 3 ros2 launch mypkg talk_listen.launch.py > tee - /tmp/mypkg.log
+dir=~
+colcon build
+source $dir/.bashrc
+timeout 3 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-cat /tmp/mypkg.log | 
-	grep 'Lis: 0'
+cat /tmp/mypkg.log | grep 'Lis: 0'
 
 
