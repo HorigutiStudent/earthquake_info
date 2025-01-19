15f6cdd75ed224ed9ec4139811f56bc30292d2ce
HorigutiStudent <s23c1127lc@s.chibakoudai.jp>
2025-01-19 18:27:47 +0900

    update

--- test/test.bash
+++ test/test.bash
@@ -11,7 +11,6 @@ colcon build
 source $dir/.bashrc
 source $dir/ros2_ws/install/setup.bash
 timeout 5 ros2 launch earthquake_info test.launch.py > /tmp/earthquake_info.log
-# timeout 5 ros2 run earthquake_info get_info | tee -  /tmp/earthquake_info.log
 
 cat /tmp/earthquake_info.log
 cat /tmp/earthquake_info.log | grep 'published 1:'
