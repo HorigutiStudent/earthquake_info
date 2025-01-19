2f02116788bbe3741dd47d5ea55eab103f10a1d8
HorigutiStudent <s23c1127lc@s.chibakoudai.jp>
2025-01-19 18:06:20 +0900

    ローカル環境でtest.bashを通す

--- earthquake_info/listener.py
+++ earthquake_info/listener.py
@@ -12,10 +12,12 @@ class Listener(Node):
     def __init__(self):
         super().__init__("listener")
         self.pub = self.create_subscription(Earthquake,"countup",self.cb,10)
+        self.n = 1  #カウント用
         
 
     def cb(self,msg):
-        self.get_logger().info(f'Publishing: {msg.datetime}, {msg.epicentre},{msg.max_seismic},{msg.prefecture},{msg.city},{msg.magnitude},{msg.tunami}')
+        self.get_logger().info(f'published {self.n}: {msg.datetime}, {msg.epicentre},{msg.max_seismic},{msg.prefecture},{msg.city},{msg.magnitude},{msg.tunami}')
+        self.n += 1
 
 
 def main():
--- test/test.bash
+++ test/test.bash
@@ -10,8 +10,8 @@ cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 source $dir/ros2_ws/install/setup.bash
-timeout 5 ros2 launch earthquake_info test.launch.py > /tmp/earthquake_info.log
+timeout 10 ros2 launch earthquake_info test.launch.py > /tmp/earthquake_info.log
 # timeout 5 ros2 run earthquake_info get_info | tee -  /tmp/earthquake_info.log
 
-cat /tmp/earthquake_info.log | grep 'Lis: 3'
+cat /tmp/earthquake_info.log | grep 'published 1:'
 
