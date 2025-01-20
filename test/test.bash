4d8653a410eac3cf4364ee5a9d99efdb22d11648
HorigutiStudent <s23c1127lc@s.chibakoudai.jp>
2025-01-20 13:35:56 +0900

    test

--- test/test.bash
+++ test/test.bash
@@ -5,15 +5,18 @@
 
 dir=~
 [ "$1" != "" ] && dir="$1"
-cd $dir/ros2_ws/src
-git clone git@github.com:HorigutiStudent/Earthquake_msg.git
+# cd $dir/ros2_ws/src
+# git clone git@github.com:HorigutiStudent/earthquake_msg.git
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 # source /opt/ros/humble/setup.bash
 source $dir/ros2_ws/install/setup.bash
 # pwd ## /root/ros2_ws
-ls src
+ls src  # earthquake_msg
+        # earthquake_info
+
+ls src/earthquake_msg
 # timeout 5 ros2 launch earthquake_info test.launch.py | tee - /tmp/earthquake_info.log
 
 # cat /tmp/earthquake_info.log
