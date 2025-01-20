c5f43157c671f880392a7f5aee0712db3dda75b1
HorigutiStudent <s23c1127lc@s.chibakoudai.jp>
2025-01-20 16:12:43 +0900

    test

--- test/test.bash
+++ test/test.bash
@@ -11,12 +11,13 @@ cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 # source /opt/ros/humble/setup.bash
-source $dir/ros2_ws/install/setup.bash
+source $dir/ros2_ws/install/setup.bash 
 # pwd ## /root/ros2_ws
 ls src  # earthquake_msg
         # earthquake_info
 
 ls src/earthquake_info/earthquake_info
+
 timeout 5 ros2 launch earthquake_info test.launch.py | tee - /tmp/earthquake_info.log
 
 cat /tmp/earthquake_info.log
