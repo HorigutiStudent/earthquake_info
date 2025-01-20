c5aeb6365473f839113d5ccb1c09546096e796d5
HorigutiStudent <s23c1127lc@s.chibakoudai.jp>
2025-01-20 19:03:24 +0900

    test

--- test/test.bash
+++ test/test.bash
@@ -11,21 +11,21 @@ source /opt/ros/humble/setup.bash
 
 cd $dir/ros2_ws
 colcon build
-# source $dir/.bashrc
+source $dir/.bashrc
 
 
-# source $dir/ros2_ws/install/setup.bash
-# source $dir/ros2_ws/install/local_setup.bash
+source $dir/ros2_ws/install/setup.bash
+source $dir/ros2_ws/install/local_setup.bash
 
-# ros2 pkg list | grep earth*
-# # pwd ## /root/ros2_ws
-# # ls src  # earthquake_msg
-#         # earthquake_info
+ros2 pkg list | grep earth*
+# pwd ## /root/ros2_ws
+# ls src  # earthquake_msg
+        # earthquake_info
 
-# #ls src/earthquake_info/earthquake_info
+#ls src/earthquake_info/earthquake_info
 
-# timeout 5 ros2 launch earthquake_info test.launch.py | tee - /tmp/earthquake_info.log
+timeout 5 ros2 launch earthquake_info test.launch.py | tee - /tmp/earthquake_info.log
 
-# #cat /tmp/earthquake_info.log
-# cat /tmp/earthquake_info.log | grep 'published 1:'
+#cat /tmp/earthquake_info.log
+cat /tmp/earthquake_info.log | grep 'published 1:'
 
